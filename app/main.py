b8d9842fba6388b8367cb54575b126953bd84348
LexingtonStanley <lexdeeley@gmail.com>
2025-04-19 14:34:09 +0100

    1st marketing commit

--- app/main.py
+++ app/main.py
@@ -2,16 +2,19 @@ from nicegui import ui, app
 from datetime import datetime, timedelta
 import os
 
-from app.pages.coming_soon import coming_soon
+from app.pages.coming_soon import coming_soon, static_path
 
 ui.add_head_html('''
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=falling" />
 ''')
 
+# Add static files here in main.py, not in the router
+app.add_static_files('/static', static_path)
+
 @ui.page('/')
 def index():
-    print('hello')
+    print('hello2')
     ui.navigate.to('/coming_soon')
 
 app.include_router(coming_soon)
--- app/pages/coming_soon.py
+++ app/pages/coming_soon.py
@@ -1,12 +1,17 @@
 from nicegui import ui, app, APIRouter
 from datetime import datetime, timedelta
 import os
-#from app.helper_funcs.DirectoryFinder import DirectoryFinder as DF
+
+from pandas.core.dtypes.common import classes
+
+from app.helper_funcs.DirectoryFinder import DirectoryFinder
 
 coming_soon = APIRouter()
-#static_path = DF().get_data_dir('static', create_if_missing=False, project_markers=['main.py'])
+df = DirectoryFinder()
+static_path = df.get_data_dir('static', create_if_missing=True, project_markers=['main.py'])
 
-#app.add_media_files('/static', static_path)
+# This should be in main.py, not in the router file
+# app.add_static_files('/static', static_path)
 
 @coming_soon.page('/coming_soon', dark=True)
 def create_coming_soon():
@@ -24,9 +29,6 @@ def create_coming_soon():
     with ui.element('div').style(
             'width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center; padding: 2rem;'
     ):
-        #ui.video('static/tandemexit_sunset_clouds.mov', autoplay=True, controls=False, loop=True)
-        ui.label('Coming Soon...')
-        print('hello')
-
-#if __name__ in {"__main__", "__mp_main__"}:
- #   print(static_path)
+        ui.video('/static/tandemexit_sunset_clouds.mov', autoplay=True, controls=False, loop=True),classes('w-full h-full')
+        ui.label('Coming Soon...').style('position: absolute; color: white; font-size: 2rem; font-weight: bold;')
+        print('hello3')
